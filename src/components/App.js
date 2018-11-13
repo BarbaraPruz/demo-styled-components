61a5d8ee020d3569872090aa06e1070dc774bde2
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-11-13 15:12:14 -0800

    Add license and set up app container component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import OptionsMenu from './options_menu';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Styled Components Demo</h1>
+        <h4>See different features of Styled Components</h4>
+        <OptionsMenu />
+      </div>
+    );
+  }
+}
+
+export default App;
