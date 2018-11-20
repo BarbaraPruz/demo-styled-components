b2e86f3711b72db6c5aa62c18b484afa9591b2fb
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-11-20 12:47:17 -0800

    Start to style main page menu

--- src/components/options_menu.js
+++ src/components/options_menu.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+//import { Button } from 'semantic-ui-react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex; 
+    justify-content: center;   
+`;
+// const Column = styled.div`
+//     flex: 1;
+//     text-align: center;  
+// `;
+const Column = styled.div `
+`;
+
+const LinkButton = styled.link`
+    display: 'block'; 
+    height: '100%';
+    background-color:blue;
+    color: white;
+`;
 export default () => {
     return (
         <div>
-            options menu
-            <Link to="/basics">Basics</Link>
-            <Link to="/themes">Themes</Link>
-        </div>
+            <h1>React Styled Components Demo</h1>
+            <Container>
+                <Link primary to="/basics">
+                    <button>Basics</button>
+                </Link>
+                <Link primary to="/themes">
+                    <button>Themes</button>
+                </Link>                
+            </Container>  
+        </div>     
     )
 }
