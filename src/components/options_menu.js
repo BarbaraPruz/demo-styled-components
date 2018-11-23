3599158cd045998d2f7aaee0a8aa678e3fa4b0d2
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-11-22 17:21:55 -0800

    Add colors

--- src/components/options_menu.js
+++ src/components/options_menu.js
@@ -11,15 +11,29 @@ const ButtonsContainer = styled.div`
 
 const Title = styled.h1`
     text-align: center;
+    font-size: 3em;
 `;
+
+const StyledLinkButton = styled(LinkButton)`
+    background-color: #DB7093;
+    color: #fff; 
+
+    display: inline-block;
+    margin: 10px;
+    -webkit-border-radius: 8px;
+    -moz-border-radius: 8px;
+    border-radius: 8px;
+    font-size: 2em;
+}   
+`;  
 
 export default () => {
     return (
         <div>
             <Title>React Styled Components Demo</Title>
             <ButtonsContainer>
-                <LinkButton to="/basics">Basics</LinkButton>
-                <LinkButton to="/themes">Themes</LinkButton>                                           
+                <StyledLinkButton to="/basics">Basics</StyledLinkButton>
+                <StyledLinkButton to="/themes">Themes</StyledLinkButton>                                           
             </ButtonsContainer>  
         </div>     
     )
